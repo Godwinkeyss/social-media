01ca826bcdd2989010182de91e723bcda7c7f5aa
Your Name <you@example.com>
2024-02-24 11:18:08 +0100

    get-a-single-user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,5 +15,22 @@ router.get('/', async(req,res)=>{
 
    } 
 })
+// get single user
+router.get('/:id', async(req,res)=>{
+   try{
+    const id = req.params.id
+      const user = await User.findById(id)
+    if(!user){
+        res.status(404).json('no user found')
+    }else{
+        const { password, updatedAt, ...other } = user._doc;
+        res.status(200).json(other)
+    }
+
+   }catch(err){
+    res.status(500).json('something went wrong')
+
+   } 
+})
 
 module.exports = router
